b17cb44b32366c773de42d5e70acf9785e269154
Ken Dale <ken@kendaleiv.com>
2016-01-06 08:11:46 -0500

    Refactor ReleaseViewModel into separate file

--- src/Site/ViewModels/Releases/IndexViewModel.cs
+++ src/Site/ViewModels/Releases/IndexViewModel.cs
@@ -1,72 +1,19 @@
 using System.Linq;
 using System.Collections.Generic;
-using RimDev.Releases.Infrastructure.GitHub;
 using RimDev.Releases.Models;
 
 namespace RimDev.Releases.ViewModels.Releases
 {
     public class IndexViewModel
-	{
-		public IndexViewModel()
-		{
+    {
+        public IndexViewModel()
+        {
             Releases = new List<ReleaseViewModel>();
         }
 
         public AppSettings AppSettings {get;set;}
-		public IList<ReleaseViewModel> Releases {get;set;}
-        
+        public IList<ReleaseViewModel> Releases {get;set;}
+
         public bool NotEmpty => Releases != null && Releases.Any();
-	}
-
-    public class ReleaseViewModel
-    {
-		public ReleaseViewModel(
-			GitHubRepository gitHubRepository,
-			Release release)
-		{
-			GitHubRepository = gitHubRepository;
-			Release = release;	
-		}
-		
-		public GitHubRepository GitHubRepository{get;protected set;}
-		public Release Release {get;protected set;}
-
-        public string FullName => GitHubRepository.FullName;
-        public string Description => GitHubRepository.Description;
-        public string Title => Release?.Name;
-
-        public string Body => Release?.Body;
-        public string CreatedAt => (Release != null) ? Release.CreatedAt.Date.ToShortDateString() : "n/a";
-		
-		public bool HasRelease => Release != null;
-
-        public string GetReleaseStatus()
-        {
-            if (HasRelease)
-            {
-                if (Release.Draft)
-                {
-                    return "Draft";
-                }
-                else if (Release.Prerelease)
-                {
-                    return "Pre-release";
-                }
-                else
-                {
-                    return "Release";
-                }
-            }
-            else
-            {
-                return "Unknown";
-            }
-        }
-
-        public bool IsDraft => HasRelease ? Release.Draft : false;
-
-        public bool IsRelease => HasRelease ? !Release.Draft && !Release.Prerelease : false;
-
-        public bool IsPreRelease => HasRelease ? Release.Prerelease : false;        
     }
 }
---
+++ src/Site/ViewModels/Releases/ReleaseViewModel.cs
@@ -0,0 +1,57 @@
+using RimDev.Releases.Infrastructure.GitHub;
+using RimDev.Releases.Models;
+
+namespace RimDev.Releases.ViewModels.Releases
+{
+    public class ReleaseViewModel
+    {
+        public ReleaseViewModel(
+            GitHubRepository gitHubRepository,
+            Release release)
+        {
+            GitHubRepository = gitHubRepository;
+            Release = release;
+        }
+
+        public GitHubRepository GitHubRepository{get;protected set;}
+        public Release Release {get;protected set;}
+
+        public string FullName => GitHubRepository.FullName;
+        public string Description => GitHubRepository.Description;
+        public string Title => Release?.Name;
+
+        public string Body => Release?.Body;
+        public string CreatedAt => (Release != null) ? Release.CreatedAt.Date.ToShortDateString() : "n/a";
+
+        public bool HasRelease => Release != null;
+
+        public string GetReleaseStatus()
+        {
+            if (HasRelease)
+            {
+                if (Release.Draft)
+                {
+                    return "Draft";
+                }
+                else if (Release.Prerelease)
+                {
+                    return "Pre-release";
+                }
+                else
+                {
+                    return "Release";
+                }
+            }
+            else
+            {
+                return "Unknown";
+            }
+        }
+
+        public bool IsDraft => HasRelease ? Release.Draft : false;
+
+        public bool IsRelease => HasRelease ? !Release.Draft && !Release.Prerelease : false;
+
+        public bool IsPreRelease => HasRelease ? Release.Prerelease : false;
+    }
+}
